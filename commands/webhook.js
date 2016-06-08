67fa2bda8b462e8236fe1dbac7680f921205c7f9
Alan H <feildmaster@users.noreply.github.com>
2016-06-08 17:55:11 -0500

    Add a method to edit the URL of a webhook

--- commands/webhook.js
+++ commands/webhook.js
@@ -7,11 +7,29 @@ function Command() {
 Command.prototype.process = function(context, app) {
   // What if we want to edit a webhook? Too freaking bad
   var trello = app.trello; // trello.get|post|put|del(uri, [query], callback)
+  var sendMessage = app._AKP48.sendMessage;
   var model, desc;
   var args = context.args;
   if (!args.length) { // We should be storing webhooks and returning the ids of webhooks that have been created
     return;
-  } else if (args.length === 1) { // We only have a model
+  }
+  
+  switch(args[0].toLowerCase()) {
+    case 'url':
+    case 'callback':
+      if (args.length !== 3) {
+        return sendMessage(`Command syntax: ${args[0]} <webhook id> <url>`, context);
+      }
+      return trello.post(`webhooks/${args[1]}/callbackURL`, {value: args[2]}, function (err, data) {
+        if (err) {
+          // TODO: send error response
+          return global.logger.error("Error while modifying webhook callbackURL", err);
+        }
+        sendMessage(JSON.stringify(data), context); // TODO: Update message
+      });
+  }
+  
+  if (args.length === 1) { // We only have a model
     model = args[0];
   } else if (args.length >= 2) { // We have a model and description
     model = args.shift();
@@ -28,7 +46,7 @@ Command.prototype.process = function(context, app) {
       return global.logger.error("Error while adding webhook", err);
     }
     global.logger.info(JSON.stringify(data));
-    app._AKP48.sendMessage(JSON.stringify(data), context);
+    sendMessage(JSON.stringify(data), context);
   });
 };
 
